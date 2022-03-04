6ddfc8a6afe5df7d9dbd41aeb80f99953d6c1870
Justin-JKim <justinjkim@cmail.carleton.ca>
2022-03-04 16:10:00 -0500

    removed main method

--- src/main/java/spring/classes/AddressBook.java
+++ src/main/java/spring/classes/AddressBook.java
@@ -32,15 +32,4 @@ public class AddressBook extends DefaultListModel<BuddyInfo> {
     public int getSize() {
         return this.size();
     }
-
-    public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-        BuddyInfo buddy1 = new BuddyInfo("Noah", "Ottawa", "613");
-        addressBook.addBuddy(buddy1);
-        BuddyInfo buddy2 = new BuddyInfo("Tim", "Toronto", "613");
-        addressBook.addBuddy(buddy2);
-        BuddyInfo buddy3 = new BuddyInfo("Dan", "Toronto", "613");
-        addressBook.addBuddy(buddy3);
-        addressBook.printBuddyInfo();
-    }
 }
