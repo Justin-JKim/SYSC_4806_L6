916856a07c7fc5b3a45529904b4592a0c4350898
Justin-JKim <justinjkim@cmail.carleton.ca>
2022-03-04 17:14:25 -0500

    removed logger

--- src/main/java/spring/AddressBookApplication.java
+++ src/main/java/spring/AddressBookApplication.java
@@ -1,18 +1,10 @@
 package spring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import spring.classes.BuddyInfo;
 
 @SpringBootApplication
 public class AddressBookApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(AddressBookApplication.class);
 
     public static void main(String args[]){
         SpringApplication.run(AddressBookApplication.class, args);
