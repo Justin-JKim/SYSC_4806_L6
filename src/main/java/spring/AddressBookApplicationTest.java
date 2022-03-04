5b13336fe186c25dfda679c06bfe4061fb81615b
Justin-JKim <justinjkim@cmail.carleton.ca>
2022-03-04 16:00:01 -0500

    Changed default address book entries

---
+++ src/main/java/spring/AddressBookApplicationTest.java
@@ -0,0 +1,14 @@
+package spring;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+public class AddressBookApplicationTest {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
