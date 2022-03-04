2896cef36d84ad088fd90a2592104624e8b91636
Justin-JKim <justinjkim@cmail.carleton.ca>
2022-03-04 17:16:08 -0500

    re added default entry and logging

--- src/main/java/spring/AddressBookApplication.java
+++ src/main/java/spring/AddressBookApplication.java
@@ -1,13 +1,37 @@
 package spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import spring.classes.BuddyInfo;
 
 @SpringBootApplication
 public class AddressBookApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(AddressBookApplication.class);
+
     public static void main(String args[]){
         SpringApplication.run(AddressBookApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(BuddyInfoRepository repository){
+
+        return (args) -> {
+            repository.save(new BuddyInfo("Justin", "Ottawa", "6132342141"));
+
+            log.info("Buddies found with findAll():");
+            log.info("-------------------------------");
+            for (BuddyInfo buddy : repository.findAll()) {
+                log.info(buddy.toString());
+            }
+            log.info("");
+        };
+
     }
 
 }
